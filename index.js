2a6ec4b553ddfe9d26b0e5b77a146199154806e6
Anastasia S.E <salamatina_anastasiya@mail.ru>
2022-12-20 15:11:24 +0000

    hello, teacher

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from "body-parser"
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
 
 app.get("/", (req, res) =>
 {
